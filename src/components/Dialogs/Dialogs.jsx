f70fdd330692ea1eb3a2508d990ff013294cdd19
temakir <vhfuk3eet3cl@mail.ru>
2021-09-22 14:40:21 +0300

    Edit: Create Dialogs.jsx content

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog+' '+s.active}>
+                    Dmitry
+                </div>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+                <div className={s.dialog}>
+                    Vikor
+                </div>
+                <div className={s.dialog}>
+                    Roman
+                </div>
+                <div className={s.dialog}>
+                    Valentina
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Yo</div>
+                <div className={s.message}>By</div>
+            </div>
         </div>
     )
 }
