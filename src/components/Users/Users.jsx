fb5c1d2f5eccd96f59eb573301036919c8772f49
temakir <vhfuk3eet3cl@mail.ru>
2021-12-08 16:38:47 +0300

    Add: pagination on UsersPage

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,18 +6,39 @@ import emptyAvatar from "../../assets/images/emptyAvatar.png";
 class Users extends React.Component {
 
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => this.props.setUsers(response.data.items))
     }
 
     render() {
+        let pageCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let page = [];
+        for (let i = 1; i <= pageCount && page.length < 18; i++) {
+            /*if () {*/
+                page.push(i)
+           /* }*/
+        }
+
         return (
             <div>
+                <div className={s.pagination}>
+                    {page.map(p => <span className={this.props.currentPage === p && s.selectedPage}
+                                         onClick={() => {this.onPageChanged(p)}}>{p} </span>)}
+                </div>
                 {this.props.users.map(u =>
                     <div className={s.userslist} key={u.id}>
 
                         <div>
-                            <img src={u.photos.small != null ? u.photos.small : emptyAvatar}
+                            <img alt='avatar' src={u.photos.small != null ? u.photos.small : emptyAvatar}
                                  className={s.photoUserList}/>
                             {u.followed
                                 ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>
--- src/redux/usersPage-reducer.js
+++ src/redux/usersPage-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialiseStore = {
-    users: []
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage:1
 }
 
 const usersPageReducer = (state = initialiseStore, action) => {
@@ -34,9 +39,18 @@ const usersPageReducer = (state = initialiseStore, action) => {
         }
         case SET_USERS: {
             return {
-                ...state, users: [...action.users]
+                ...state, users: action.users
             }
-
+        }
+        case SET_CURRENT_PAGE: {
+            return {
+                ...state, currentPage: action.pageNumber
+            }
+        }
+        case SET_TOTAL_USERS_COUNT: {
+            return {
+                ...state, totalUsersCount: action.count
+            }
         }
         default :
             return state
@@ -46,6 +60,8 @@ const usersPageReducer = (state = initialiseStore, action) => {
 export const followAC = (userId) => ({type: FOLLOW, userId});
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
 export const setUsersAC = (users) => ({type: SET_USERS, users});
+export const setCurrentPageAC = (pageNumber) => ({type: SET_CURRENT_PAGE, pageNumber});
+export const setTotalUsersCountAC = (totalCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalCount});
 
 
 export default usersPageReducer;
